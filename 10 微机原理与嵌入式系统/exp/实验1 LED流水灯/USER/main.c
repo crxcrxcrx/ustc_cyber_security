6797c976f9b3af22da28f7cb27f499b18fa5ff6a
yssickjgd <yssickjgd@mail.ustc.edu.cn>
2022-05-15 17:46:55 +0800

    更改微嵌实验内容

--- 10 微机原理与嵌入式系统/exp/实验1 LED流水灯/USER/main.c
+++ 10 微机原理与嵌入式系统/exp/实验1 LED流水灯/USER/main.c
@@ -2,6 +2,7 @@
 #include "delay.h"//延时函数所在的头文件
 
 void LED_Init(void); 		
+void control(uint8_t c);
 
 #define ON   Bit_SET     //GPIO的布尔变量，用于GPIO_WriteBit函数
 #define OFF  Bit_RESET   //GPIO的布尔变量，用于GPIO_WriteBit函数
@@ -23,14 +24,22 @@ int main(void)
 	while(1)
 	{
 		/*流水灯显示*/
-		LED0(ON);delay_ms(500);LED0(OFF);delay_ms(500);
-		LED1(ON);delay_ms(500);LED1(OFF);delay_ms(500);
-		LED2(ON);delay_ms(500);LED2(OFF);delay_ms(500);
-		LED3(ON);delay_ms(500);LED3(OFF);delay_ms(500);
-		LED4(ON);delay_ms(500);LED4(OFF);delay_ms(500);
-		LED5(ON);delay_ms(500);LED5(OFF);delay_ms(500);
-		LED6(ON);delay_ms(500);LED6(OFF);delay_ms(500);
-		LED7(ON);delay_ms(500);LED7(OFF);delay_ms(500);      
+		control(1);
+		delay_ms(500);
+		control(3);
+		delay_ms(500);
+		control(7);
+		delay_ms(500);
+		control(15);
+		delay_ms(500);
+		control(31);
+		delay_ms(500);
+		control(63);
+		delay_ms(500);
+		control(127);
+		delay_ms(500);
+		control(255);
+		delay_ms(500);
  	}
 }
 void LED_Init(void)
@@ -54,5 +63,41 @@ void LED_Init(void)
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
 	GPIO_ResetBits(GPIOD, GPIO_Pin_7 |GPIO_Pin_12|GPIO_Pin_13);  //LED6,LED7		 
+}
+
+void control(uint8_t c)
+{
+	if((c & 1) == 1) 
+		LED0(ON);
+	else
+		LED0(OFF);
+	if((c & 2) == 2)
+		LED1(ON);
+	else
+		LED1(OFF);
+	if((c & 4) == 4)
+		LED2(ON);
+	else
+		LED2(OFF);
+	if((c & 8) == 8)
+		LED3(ON);
+	else
+		LED3(OFF);
+	if((c & 16) == 16)
+		LED4(ON);
+	else
+		LED4(OFF);
+	if((c & 32) == 32)
+		LED5(ON);
+	else
+		LED5(OFF);
+	if((c & 64) == 64)
+		LED6(ON);
+	else
+		LED6(OFF);
+	if((c & 128) == 128)
+		LED7(ON);
+	else
+		LED7(OFF);
 }
 
